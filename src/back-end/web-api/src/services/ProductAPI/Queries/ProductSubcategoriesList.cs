2055141c6a610ebd7c7bab6611e30f73e70414c6
Duong Thanh Nam <thanhnam.rei@gmail.com>
2023-05-24 15:38:53 +0700

    refactor: create docker componse

--- src/back-end/web-api/src/services/ProductAPI/Queries/ProductSubcategoriesList.cs
+++ src/back-end/web-api/src/services/ProductAPI/Queries/ProductSubcategoriesList.cs
@@ -1,11 +1,6 @@
 ﻿using Domain.Entities;
 using MediatR;
 using ProductAPI.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProductAPI.Queries;
 
